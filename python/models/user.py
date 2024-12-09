86b50a7bf6f79645471c18b86ede41fdc0536b78
d11m08y03 <zakariyyakurmally@gmail.com>
2024-12-09 21:03:24 +0400

    Add ORM models

---
+++ python/models/user.py
@@ -0,0 +1,8 @@
+from database import db
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.Integer(255), nullable=False)
